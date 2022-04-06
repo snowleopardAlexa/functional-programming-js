73f8feb31fc9ece2974209ccf03aee17a55b6343
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-06 17:09:45 -0400

    add code

--- hello-world.js
+++ hello-world.js
@@ -73,3 +73,75 @@ const updatedEmployee1= {
 // we start from immutable set of data as a single source of truth
 // and then we add functions to combine this data and transform it into an information.
 // original data is INTACT! --> you can't change it!
+
+// SEPARATION OF DATA AND FUNCTIONS
+
+// array of data objects
+const data = [
+    {
+        id: 1,
+        name: "Alexa",
+        address: "123 Easy Street",
+        ITIN: 123444,
+    }
+]
+
+// F - data is represented by arrays of objects
+// OOP - data is usually wrapped up inside objects as member 
+// variables, and the only way we can access it is using an object's methods. 
+
+
+// F
+const carsArray = [...]
+const newCars = filter(cars, 2018)
+
+// OOP
+class CarList {
+    ...
+    findNewCars(sinceYear) {
+        ...
+    }
+}
+
+//  In order to operate on given data, the data must be passed as arguments to 
+// the function instead of using the this keyword as in object-oriented programming. 
+// And because of the rule of immutability, they should never make changes to any of 
+// the data they touch. They only return a modified copy of that data.
+
+// OOP - why we keep data and functions together?
+// The main reason that it's useful to keep data and functions together in 
+// the same object is that this allows us to interact with the member variables
+//  of an object without touching them directly.
+// WHAT'S WRONG WITH TOUCHING VARIABLES DIRECTLY?
+// DIRECT ACCESS TO VARIABLES --> programmers will change them in the way they are not supposed to
+// variables can be made private in JS - it is done by adding an underscore before the variables -->
+// they can be only accesed by functions now
+
+class Cat {
+    constructor(name, gender) {
+        this._name = name
+        this._gender = gender
+        this._initials = `${name.charAt(0)}${lastName.charAt(0)}`
+    }
+
+    setName(newName) {
+        this._firstName = newName
+        this._initials = `${this._name.charAt(0)}${this._gender.charAt(0)}`
+    }
+}
+
+// F - programmers can't ALTER data in unwanted ways
+// it is against immutability - the data remains a constant source of truth
+// if we want to change some's name, we define a new constant that represents the updated data
+// Then we use this updated constant in all our future calculations, knowing exactly what data it represents
+
+const createCat = (name, gender) => ({
+    name,
+    gender,
+    initials: `${name.charAt(0)}${gender.charAt(0)}`
+})
+const cat1 = createCat('Kion', 'male')
+const updatedCat1 = createCat('Nala', cat1.gender)
+
+
+// FIRST CLASS FUNCTIONS
